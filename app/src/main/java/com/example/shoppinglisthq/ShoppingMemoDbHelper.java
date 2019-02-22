ae35aac5097f19d90cc823e7c85a68227ac8ecb1
Frank <f.neumann.berlin@googlemail.com>
2019-02-22 15:54:31 +0100

    Tag1

--- app/src/main/java/com/example/shoppinglisthq/ShoppingMemoDbHelper.java
+++ app/src/main/java/com/example/shoppinglisthq/ShoppingMemoDbHelper.java
@@ -1,7 +1,7 @@
 package com.example.shoppinglisthq;
 
 import android.content.Context;
-import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase; // leer
 import android.database.sqlite.SQLiteOpenHelper;
 import android.support.annotation.Nullable;
 import android.util.Log;
