034741b19711231ee2eba55a479f19ed0fb409a1
Frank <f.neumann.berlin@googlemail.com>
2019-02-26 15:54:15 +0100

    Tag3

--- app/src/main/java/com/example/shoppinglisthq/MainActivity.java
+++ app/src/main/java/com/example/shoppinglisthq/MainActivity.java
@@ -1,36 +1,303 @@
 package com.example.shoppinglisthq;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AbsListView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
     ShoppingMemoDataSource dataSource;
+    private boolean isButtonClick = true;
+    private ListView mShoppingMemosListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ShoppingMemo testMemo = new ShoppingMemo("Birnen",5,100);
-        Log.d(TAG, "Inhalt der TestMemo " + testMemo.toString());
-
         dataSource = new ShoppingMemoDataSource(this);
+
+        initializeShoppingMemosListView();
+
+        activateAddButton();
+        initializeContextualActionBar();
+    }
+
+    private void initializeShoppingMemosListView() {
+        List<ShoppingMemo> emptyListForInitialisation = new ArrayList<>();
+
+        mShoppingMemosListView = findViewById(R.id.listview_shopping_memos);
+        ArrayAdapter<ShoppingMemo> shoppingMemoArrayAdapter = new ArrayAdapter<ShoppingMemo>(this,
+                android.R.layout.simple_list_item_multiple_choice,emptyListForInitialisation){
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                View view = super.getView(position,convertView,parent);
+                TextView textView = (TextView)view;
+
+                ShoppingMemo memo = (ShoppingMemo) mShoppingMemosListView.getItemAtPosition(position);
+                if(memo.isChecked()){
+                    textView.setPaintFlags(textView.getPaintFlags()| Paint.STRIKE_THRU_TEXT_FLAG);
+                    textView.setTextColor(Color.rgb(175,175,175));
+                }else{
+                    textView.setPaintFlags(textView.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                    textView.setTextColor(Color.DKGRAY);
+                }
+
+                return view;
+            }
+        };
+
+        mShoppingMemosListView.setAdapter(shoppingMemoArrayAdapter);
+    }
+
+
+    private void activateAddButton() {
+
+        final EditText editTextQuantity = findViewById(R.id.editText_quantity);
+        final EditText editTextProduct = findViewById(R.id.editText_product);
+        Button buttonAddProduct = findViewById(R.id.button_add_prduct);
+        buttonAddProduct.setOnClickListener(v -> {
+
+            String quantityString = editTextQuantity.getText().toString();
+            String product = editTextProduct.getText().toString();
+
+            if (TextUtils.isEmpty(quantityString)) {
+                editTextQuantity.setError(getString(R.string.editText_errorMessage));
+                return;
+            }
+            if (TextUtils.isEmpty(product)) {
+                editTextProduct.setError(getString(R.string.editText_errorMessage));
+                return;
+            }
+
+            int quantity = Integer.parseInt(quantityString);
+            editTextProduct.setText("");
+            editTextQuantity.setText("");
+
+            dataSource.createShoppingMemo(product, quantity);
+
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if (getCurrentFocus() != null && isButtonClick) {
+                inputMethodManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+
+            }
+
+            showAllListEntries();
+        });
+        editTextProduct.setOnEditorActionListener((textView, pos, keyEvent) -> {
+            isButtonClick = false;
+            buttonAddProduct.performClick();
+            editTextQuantity.requestFocus();
+            isButtonClick = true;
+            return true;
+        });
+    }
+
+    private void initializeContextualActionBar() {
+        final ListView shoppingMemoListView = findViewById(R.id.listview_shopping_memos);
+        shoppingMemoListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+
+        shoppingMemoListView.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+
+            int selCount = 0;
+
+            @Override
+            public void onItemCheckedStateChanged(ActionMode mode, int position, long id, boolean checked) {
+               if(checked){
+                   selCount++;
+               }else{
+                   selCount--;
+               }
+               String cabTitel = selCount + " " + getString(R.string.cab_checked_string);
+               mode.setTitle(cabTitel);
+               mode.invalidate();
+
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+                getMenuInflater().inflate(R.menu.menu_contextual_action_bar, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+                MenuItem item = menu.findItem(R.id.cab_change);
+                if(selCount == 1){
+                    item.setVisible(true);
+                }else{
+                    item.setVisible(false);
+                }
+                return true;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+                boolean returnValue = true;
+                SparseBooleanArray touchedShoppingMemosPosition = shoppingMemoListView.getCheckedItemPositions();
+                switch (item.getItemId()) {
+                    case R.id.cab_delete:
+
+                        for (int i = 0; i < touchedShoppingMemosPosition.size(); i++) {
+                            boolean isChecked = touchedShoppingMemosPosition.valueAt(i);
+                            if (isChecked) {
+                                int positionInListView = touchedShoppingMemosPosition.keyAt(i);
+                                ShoppingMemo shoppingMemo =
+                                        (ShoppingMemo) shoppingMemoListView.getItemAtPosition(positionInListView);
+                                Log.d(TAG, "Position im ListView: " + positionInListView + " Inhalt: " + shoppingMemo.toString());
+                                dataSource.deleteShoppingMemo(shoppingMemo);
+                            }
+                        }
+                        showAllListEntries();
+                        mode.finish();
+                        break;
+                    case R.id.cab_change:
+
+                        for (int i = 0; i < touchedShoppingMemosPosition.size(); i++) {
+                            boolean isChecked = touchedShoppingMemosPosition.valueAt(i);
+                            if (isChecked) {
+                                int positionInListView = touchedShoppingMemosPosition.keyAt(i);
+                                ShoppingMemo shoppingMemo =
+                                        (ShoppingMemo) shoppingMemoListView.getItemAtPosition(positionInListView);
+                                Log.d(TAG, "Position im ListView: " + positionInListView + " Inhalt: " + shoppingMemo.toString());
+                                AlertDialog editShoppingMemoDialog = createShoppingMemoDialog(shoppingMemo);
+                                editShoppingMemoDialog.show();
+//                                View forKeyboard = editShoppingMemoDialog.getCurrentFocus();
+//                                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+//                                imm.showSoftInput(getCurrentFocus(),InputMethodManager.RESULT_SHOWN);
+                            }
+                        }
+
+                                mode.finish();
+                        break;
+                    default:
+                        returnValue = false;
+                }
+
+                return returnValue;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode mode) {
+                selCount = 0;
+
+            }
+        });
+    }
+
+    private AlertDialog createShoppingMemoDialog(final ShoppingMemo shoppingMemo) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        LayoutInflater inflater = getLayoutInflater();
+
+        View dialogsView = inflater.inflate(R.layout.dialog_edit_shopping_memo, null);
+
+        final EditText editTextNewQuantity = dialogsView.findViewById(R.id.editText_new_quantity);
+        editTextNewQuantity.setText(String.valueOf(shoppingMemo.getQuantity()));
+
+
+        final EditText editTextNewProduct = dialogsView.findViewById(R.id.editText_new_product);
+        editTextNewProduct.setText(shoppingMemo.getProduct());
+
+        builder.setView(dialogsView)
+                .setTitle(R.string.dialog_titel)
+                .setPositiveButton(R.string.dialog_button_positiv, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String quantityString = editTextNewQuantity.getText().toString();
+                        String product = editTextNewProduct.getText().toString();
+
+                        if (TextUtils.isEmpty(quantityString) || TextUtils.isEmpty(product)) {
+                            Toast.makeText(MainActivity.this, "Felder dürfen nicht leer sein", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+                        int quantity = Integer.parseInt(quantityString);
+
+                        ShoppingMemo memo = dataSource.updateShoppingMemo(shoppingMemo.getId(), product, quantity);
+
+                        Log.d(TAG, "Alter Eintrag - ID: " + shoppingMemo.getId() + " Inhalt: " + shoppingMemo.toString());
+                        Log.d(TAG, "Neuer Eintrag - ID: " + memo.getId() + " Inhalt: " + memo.toString());
+
+                        showAllListEntries();
+                        dialog.dismiss();
+//                        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+//                        imm.hideSoftInputFromWindow(getCurrentFocus().getApplicationWindowToken(),InputMethodManager.HIDE_IMPLICIT_ONLY);
+
+                    }
+                })
+                .setNegativeButton(R.string.dialog_button_negativ, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+        editTextNewQuantity.setSelection(0,editTextNewQuantity.length());
+        return builder.create();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Datenquelle wird geöffnet");
+        dataSource.open();
+        showAllListEntries();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "Datenquelle wird geschlossen.");
+        dataSource.close();
+    }
+
+    private void showAllListEntries() {
+        List<ShoppingMemo> shoppingMemos = dataSource.getAllShoppingMemos();
+
+        ArrayAdapter<ShoppingMemo> shoppingMemoArrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_multiple_choice, shoppingMemos);
+
+        ListView shoppingMemoListView = findViewById(R.id.listview_shopping_memos);
+        shoppingMemoListView.setAdapter(shoppingMemoArrayAdapter);
+
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case R.id.action_settings:
                 Toast.makeText(this, "Settings wurde gedrückt", Toast.LENGTH_SHORT).show();
                 return true;
--- app/src/main/java/com/example/shoppinglisthq/ShoppingMemoDataSource.java
+++ app/src/main/java/com/example/shoppinglisthq/ShoppingMemoDataSource.java
@@ -1,8 +1,13 @@
 package com.example.shoppinglisthq;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShoppingMemoDataSource {
 
@@ -11,8 +16,103 @@ public class ShoppingMemoDataSource {
     private SQLiteDatabase database;
     private ShoppingMemoDbHelper dbHelper;
 
+    private String[] columns = {
+            ShoppingMemoDbHelper.COLUMN_ID,
+            ShoppingMemoDbHelper.COLUMN_PRODUCT,
+            ShoppingMemoDbHelper.COLUMN_QUANTITY,
+            ShoppingMemoDbHelper.COLUMN_CHECKED
+    };
+
     public ShoppingMemoDataSource(Context context){
         Log.d(TAG, "DataSource erzeugt jetzt den dbHelper");
         dbHelper = new ShoppingMemoDbHelper(context);
+    }
+
+    public ShoppingMemo createShoppingMemo(String product, int quantity){
+        ContentValues values= new ContentValues();
+        values.put(ShoppingMemoDbHelper.COLUMN_PRODUCT,product);
+        values.put(ShoppingMemoDbHelper.COLUMN_QUANTITY,quantity);
+
+        long insertId = database.insert(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,null,values);
+
+        Cursor cursor = database.query(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,columns,
+                ShoppingMemoDbHelper.COLUMN_ID + "=" + insertId,null,null,null,null);
+
+        cursor.moveToFirst();
+        ShoppingMemo shoppingMemo = cursorToShoppingMemo(cursor);
+        cursor.close();
+        return shoppingMemo;
+    }
+
+    public ShoppingMemo updateShoppingMemo(long id, String newProduct, int newQuantity, boolean newChecked){
+        int intValueChecked = newChecked? 1 : 0;
+
+        ContentValues values = new ContentValues();
+        values.put(ShoppingMemoDbHelper.COLUMN_PRODUCT,newProduct);
+        values.put(ShoppingMemoDbHelper.COLUMN_QUANTITY,newQuantity);
+        values.put(ShoppingMemoDbHelper.COLUMN_CHECKED, intValueChecked);
+
+        database.update(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,values,ShoppingMemoDbHelper.COLUMN_ID + "=" + id,
+                null);
+
+        Cursor cursor = database.query(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,columns,ShoppingMemoDbHelper.COLUMN_ID +"="+id,
+                null,null,null,null);
+        cursor.moveToFirst();
+        ShoppingMemo memo = cursorToShoppingMemo(cursor);
+        cursor.close();
+        return memo;
+    }
+
+    public void deleteShoppingMemo(ShoppingMemo shoppingMemo){
+        long id = shoppingMemo.getId();
+        database.delete(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,
+                ShoppingMemoDbHelper.COLUMN_ID + "=" + id,null);
+        Log.d(TAG, "Eintrag gelöscht! ID: " + id + " Inhalt: " + shoppingMemo.toString());
+    }
+
+    private ShoppingMemo cursorToShoppingMemo(Cursor cursor) {
+        int idIndex = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_ID);
+        int idProduct = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_PRODUCT);
+        int idQuantity = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_QUANTITY);
+        int idChecked = cursor.getColumnIndex(ShoppingMemoDbHelper.COLUMN_CHECKED);
+
+        String product = cursor.getString(idProduct);
+        int quantity = cursor.getInt(idQuantity);
+        long id = cursor.getLong(idIndex);
+        int intValueChecked = cursor.getInt(idChecked);
+
+        boolean isCkecked = intValueChecked != 0 ;
+
+        ShoppingMemo shoppingMemo = new ShoppingMemo(product,quantity,id,isCkecked);
+        return shoppingMemo;
+    }
+
+    public List<ShoppingMemo> getAllShoppingMemos(){
+        List<ShoppingMemo> shoppingMemoList = new ArrayList<>();
+        Cursor cursor = database.query(ShoppingMemoDbHelper.TABLE_SHOPPING_LIST,columns,
+                null,null,null,null,null);
+        cursor.moveToFirst();
+        ShoppingMemo shoppingMemo;
+
+        while (!cursor.isAfterLast()){
+            shoppingMemo = cursorToShoppingMemo(cursor);
+            shoppingMemoList.add(shoppingMemo);
+            Log.d(TAG, "ID: " + shoppingMemo.getId() + ", Inhalt: " + shoppingMemo.toString());
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return shoppingMemoList;
+    }
+
+
+    public void open(){
+        Log.d(TAG, "Eine Referenz auf die Datenbank wird angefragt.");
+        database = dbHelper.getWritableDatabase();
+        Log.d(TAG, "open: Referenz erhalten. Pfad zur DB: " + database.getPath());
+    }
+
+    public void close(){
+        dbHelper.close();
+        Log.d(TAG, "Datenbank mit DbHelper geschlossen");
     }
 }
